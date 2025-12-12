dd3a2bfe1fbed3389d085ed37861392e10388bb0
Pavan Kumar <kpavan36668@gmail.com>
2025-12-12 22:54:09 +0530

    solved problem number 1929 - concatenation of arrays

---
+++ java/arrays/Concatenation_of_Array_1929.java
@@ -0,0 +1,14 @@
+package java.arrays;
+
+public class Concatenation_of_Array_1929 {
+
+        public int[] getConcatenation(int[] nums) {
+        int n=nums.length;
+        int[] ans=new int[n*2];
+        for(int i=0;i<n;i++){
+            ans[i]=nums[i];
+            ans[i+n]=nums[i];
+        }
+        return ans;
+    }
+}
